30bf4c15f994e7ccced5516e623aff4374115d67
Minyus <Minyus@users.noreply.github.com>
2023-06-30 11:59:14 +0800

    Bump version to 0.0.7

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ https://github.com/Minyus/recurtx
 
 setup(
     name="recurtx",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages(exclude=["tests"]),
     entry_points={"console_scripts": console_scripts},
     install_requires=requires,
