ba81721429f85ddd298fbac82699325ea174137b
Minyus <Minyus@github.com>
2024-02-14 08:24:44 +0800

    Bump up version to 0.0.15

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ https://github.com/Minyus/recurtx
 
 setup(
     name="recurtx",
-    version="0.0.14",
+    version="0.0.15",
     packages=find_packages(exclude=["tests"]),
     entry_points={"console_scripts": console_scripts},
     install_requires=requires,
